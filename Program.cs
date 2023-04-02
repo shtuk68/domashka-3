ba8960338932def39d047cef8357ba7702f4c065
nikita <shtukolimp@mail.ru>
2023-04-02 06:33:41 +0300

    две задачки

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Console.Clear();
+// Console.Write ("vvedite chislo");
+// int n = int.Parse(Console.ReadLine()!);
+// while (n < 10000 || n > 99999 )
+// {
+//     Console.Write("vi oshiblisb:  ");
+//     n = int.Parse(Console.ReadLine()!);
+// }
+// int n1 = n / 10000;
+// int n2 = (n / 1000) % 10;
+// int n3 = (n  % 100) / 10;
+// int n4 = n % 10;
+// if (n1 == n4 && n2 == n3)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+
+
+
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// for (int i = 1; i <= n; i++)
+//     Console.WriteLine($"{i} ^ 3 = {i * i * i}");
